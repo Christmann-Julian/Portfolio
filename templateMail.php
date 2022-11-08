65d9370f4843cb26914762621b9a60df92ec5ceb
Christmann-Julian <julianchristmann@laposte.net>
2022-11-08 21:57:55 +0100

    nouveau template

--- templateMail.php
+++ templateMail.php
@@ -9,27 +9,35 @@ function templateMail($name, $message, $mail){
         <title>HTML email template</title>
         <style>
             *{margin: 0;padding: 0;box-sizing: border-box;}
-            body{font-family: sans-serif;min-height: 100vh;}
-            .page{width: 768px;margin:  0 auto;font-size: 16px;color: #555555;}
-            h1{background-color: #f4f4f4;padding: 20px;margin-top: 20px;}
-            h2{padding: 20px;}
-            p{padding: 10px 20px;line-height: 26px;}	
-            footer{padding: 20px;}
+            body{font-family: sans-serif;}
+            .page{max-width: 768px;margin: 0 auto;color: #555555;}
+            h1, h2, p{padding: 1rem;}
+            h1{background-color: #f4f4f4;}
+            .border{border-top: 1px solid #555555;border-bottom: 1px solid #555555;}
             footer a{text-decoration: none;color: #155197;}
         </style>
     </head>
     <body>
         <div class="page">
-            <h1>Message envoyé sur le portfolio</h1>
-            <h2>De '.$name.' le '.date("d/m/y à G:i").'</h2>
-            <p>'.$message.'</p>
-            <p><strong>Adresse mail: </strong>'.$mail.'</p>
+            <header>
+                <h1>Message envoyé sur le portfolio</h1>
+                <p>
+                    <strong>From: </strong>'.$name.'
+                    <br>
+                    <strong>Date: </strong>'.date("d/m/y à G:i").'
+                </p>
+            </header>
+            <p class="border">'.$message.'</p>
             <footer>
-                <a href="http://julianchristmann.fr/">julianchristmann.fr</a>
+                <p>
+                    <strong>Mail: </strong>'.$mail.'
+                    <br>
+                    <strong>Site: </strong><a href="http://julianchristmann.fr/">julianchristmann.fr</a>
+                </p>
             </footer>
         </div>
     </body>
-    </html>';
+</html>';
 
     return $content;
 }
